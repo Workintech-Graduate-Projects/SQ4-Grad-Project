3a36eb8a85e66d15be0414736f49678bae41e217
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-06-16 12:27:30 +0300

    PipeDrive Butonu eklendi

--- front-end/src/components/calculating/calculate.js
+++ front-end/src/components/calculating/calculate.js
@@ -1,16 +1,17 @@
-import React , { useState } from 'react';
-
-
+import React, { useState } from "react";
+import axios from "axios";
 
 function CalculateCreditScore(sectorScore, positionScore, experienceScore) {
   const sectorWeight = 0.3;
   const positionWeight = 0.4;
   const experienceWeight = 0.2;
 
-  const creditScore = (sectorScore * sectorWeight) + (positionScore * positionWeight) - (experienceScore * experienceWeight);
-  
-  return creditScore;
+  const creditScore =
+    sectorScore * sectorWeight +
+    positionScore * positionWeight -
+    experienceScore * experienceWeight;
 
+  return creditScore;
 }
 
 function getPreference(creditScore) {
@@ -23,8 +24,7 @@ function getPreference(creditScore) {
   }
 }
 
-function CreditScoreCalculator({obj}) {
-  
+function CreditScoreCalculator({ obj }) {
   const [sectorScore, setSectorScore] = useState(0);
   const [positionScore, setPositionScore] = useState(0);
   const [experienceScore, setExperienceScore] = useState(0);
@@ -32,40 +32,63 @@ function CreditScoreCalculator({obj}) {
   const [preference, setPreference] = useState(0);
 
   const newObj = obj;
-  
 
   const handleCalculate = () => {
-    const calculatedCreditScore = CalculateCreditScore(sectorScore, positionScore, experienceScore);
+    const calculatedCreditScore = CalculateCreditScore(
+      sectorScore,
+      positionScore,
+      experienceScore
+    );
     const calculatedPreference = getPreference(calculatedCreditScore);
 
     newObj.creditScore = calculatedCreditScore;
     newObj.preference = calculatedPreference;
-    console.log(newObj)
+    console.log(newObj);
     setCreditScore(calculatedCreditScore);
     setPreference(calculatedPreference);
-    
   };
+  async function handleOnclick() {
+    try {
+      await axios.post("http://localhost:9000/users", { newObj });
+      console.log("post işe yaradı");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div>
       <h1>Kredi Puanı Hesaplama</h1>
       <div>
         <label>Sektör Puanı:</label>
-        <input type="number" value={sectorScore} onChange={(e) => setSectorScore(Number(e.target.value))} />
+        <input
+          type="number"
+          value={sectorScore}
+          onChange={(e) => setSectorScore(Number(e.target.value))}
+        />
       </div>
       <div>
         <label>Pozisyon Puanı:</label>
-        <input type="number" value={positionScore} onChange={(e) => setPositionScore(Number(e.target.value))} />
+        <input
+          type="number"
+          value={positionScore}
+          onChange={(e) => setPositionScore(Number(e.target.value))}
+        />
       </div>
       <div>
         <label>Deneyim Puanı:</label>
-        <input type="number" value={experienceScore} onChange={(e) => setExperienceScore(Number(e.target.value))} />
+        <input
+          type="number"
+          value={experienceScore}
+          onChange={(e) => setExperienceScore(Number(e.target.value))}
+        />
       </div>
       <button onClick={handleCalculate}>Hesapla</button>
       <div>
         <p>Kredi Puanı: {creditScore}</p>
         <p>Tercih Sırası: {preference}</p>
       </div>
+      <button onClick={handleOnclick}>PipeDrive Aktar </button>
     </div>
   );
 }
