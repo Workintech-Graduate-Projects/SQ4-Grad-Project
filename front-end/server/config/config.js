1928f8c2f53bcf629b84a217e230eaccaa052016
Taner Aytaş <120183459+taneraytas@users.noreply.github.com>
2023-06-21 15:53:49 +0300

    Backend ayrıldı

--- front-end/server/config/config.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require("dotenv").config();
-module.exports = {
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV || "development",
-  //TYPEFORM_TOKEN: process.env.NODE_ENV,
-  pdTOKEN: process.env.PIPEDRIVE_TOKEN,
-};
