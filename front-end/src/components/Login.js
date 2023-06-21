64ed96dc7fa0496621b693b77929eb820b7d1345
berkakaz <berk.akaz@gmail.com>
2023-06-21 23:26:39 +0300

    login sayfası düzenleme

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -19,23 +19,46 @@ function Login() {
   };
 
   return (
+    
+    <div className="mx-96 drop-shadow-lg">
+        <label htmlFor="name-input"><h2 className="font-bold mt-24 text-[18px]">Panel Girişi</h2></label>
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="username"
-        value={username}
-        onChange={handleUsernameChange}
-      />
+      <div className="mt-6">
+      <label htmlFor="name-input" class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Kullanıcı Adı :</label>
+      <input type="text" 
+      id="text" 
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+      placeholder="Kullanıcı Adı" required
+      value={username}
+        onChange={handleUsernameChange}></input>
+      </div>
+      <div className="mt-8 ">
+      <label for="password" class="block mb-2 text-sm text-left font-medium text-gray-900 dark:text-white">Parola :</label>
       <input
         type="password"
         placeholder="Parola"
+        id="password" 
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
         value={password}
         onChange={handlePasswordChange}
       />
+      </div>
+      <div class="flex items-start mt-6">
+    <div class="flex items-center h-5">
+      <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required>
+        </input>
+    </div>
+    <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Beni Hatırla</label>
+  </div>
+    <div className="mt-6 ">
       <Link to="/login">
-        <button type="submit">Giriş Yap</button>
+        <button type="submit"
+        className="py-2 px-4 shadow-xl bg-blue-500 text-white font-semibold rounded-full mt-4 shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Giriş Yap</button>
       </Link>
+      </div>
     </form>
+    </div>
+    
   );
 }
 
--- front-end/src/components/fetTypeFrom.js
+++ front-end/src/components/fetTypeFrom.js
@@ -21,7 +21,8 @@ function TypeFormFetcher() {
   return (
     <>
 
-      <button onClick={handleOnClick}>Type Formdan Data Çek</button>
+      <button onClick={handleOnClick}
+      className="py-2 px-4 shadow-xl bg-blue-500 text-white font-semibold rounded-full mt-4 shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">TypeForm'dan Data Çek</button>
       {/*allAnswers === null ? (
 
       <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 mt-7 mb-7 border-b-4 border-blue-700 hover:border-blue-500 shadow-lg shadow-blue-500/50 rounded-full transition  delay-100  hover:scale-110 duration-300" onClick={handleOnClick}>Typeform'dan Data Çek</button>
